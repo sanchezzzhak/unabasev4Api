d0d460553346a340e8b281c167683270b5617f9e
Simon Gomes <xtgomes@gmail.com>
2018-11-01 18:22:50 -0300

    debug cors

--- src/app.js
+++ src/app.js
@@ -97,9 +97,9 @@ app.use(function(req, res, next) {
   res.locals.host = mainConfig.host;
   let origin = req.headers.origin;
 
-  // if (allowedOrigins.indexOf(origin) > -1) {
-  // res.header('Access-Control-Allow-Origin', origin);
-  // }
+  if (allowedOrigins.indexOf(origin) > -1) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
   // res.header('Access-Control-Allow-Origin', true);
   res.header('Access-Control-Allow-Origin', '*');
   // res.header('Access-Control-Allow-Origin', mainConfig.web);
