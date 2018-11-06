d89fe0589a1fe666fc2b9a2d67e8769a13d1db84
Simon Gomes <xtgomes@gmail.com>
2018-11-06 13:19:58 -0300

    debug envar

--- src/app.js
+++ src/app.js
@@ -103,7 +103,7 @@ app.use(function(req, res, next) {
   // res.header('Access-Control-Allow-Origin', true);
   res.header('Access-Control-Allow-Origin', '*');
   // res.header('Access-Control-Allow-Origin', mainConfig.web);
-  res.header('Access-Control-Allow-Credentials', true);
+  // res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', [
     'POST',
     'GET',
