0c2f6c361eeae555b230b4db9e1d012169c354e0
Simon Gomes <xtgomes@gmail.com>
2018-11-12 16:36:09 -0300

    add restartPassword template languages

--- src/lib/mails/modules/restartPassword.js
+++ src/lib/mails/modules/restartPassword.js
@@ -1,10 +1,25 @@
 import main from '../main';
-const restartPassword = ({ origin = '' }) => {
-  return main({
-    content: `
-     Has solicitado reiniciar tu contraseña
-    `
-  });
+const restartPassword = ({ origin = '', lang = 'es', id = '' }) => {
+  let text;
+  switch (lang) {
+    case 'es':
+      text = {
+        content: `<p style="font-size=1.5rem">
+        Has solitado reestablecer tu contraseña,
+        sigue el siguiente <a href="${origin}/restart?id=${id}">Enlace</a> para establecer una nueva.
+      </p>`
+      };
+      break;
+    case 'en':
+      text = {
+        content: `<p style="font-size=1.5rem">
+        You have requested to reset your password,
+        follow the  <a href="${origin}/restart?id=${id}">Link</a> to set a new one.
+      </p>`
+      };
+      break;
+  }
+  return main(text);
 };
 
 export default restartPassword;
