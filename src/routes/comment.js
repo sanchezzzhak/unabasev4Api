e13c1eff63641f6b40fc1e4c91a249bbd282b3d2
Simon Gomes <xtgomes@gmail.com>
2018-11-28 19:29:34 -0300

    add auth to comment routes

--- src/routes/comment.js
+++ src/routes/comment.js
@@ -3,6 +3,8 @@ import { Router } from 'express';
 const comment = Router();
 let module = 'comment';
 
+import auth from '../config/lib/auth';
+comment.use(auth.sToken);
 comment.post('/', create);
 comment.delete('/:id', deleteOne);
 comment.get('/:id/:name', getFrom);
