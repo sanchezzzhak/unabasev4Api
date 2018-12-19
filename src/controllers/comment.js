23c29960da0d98c413ac778aaaca85b57cce66ec
Simon Gomes <xtgomes@gmail.com>
2018-12-19 15:16:54 -0300

    sort comment from

--- src/controllers/comment.js
+++ src/controllers/comment.js
@@ -39,6 +39,9 @@ export const getFrom = (req, res) => {
   console.log(req.params);
   Comment.find({ 'from.id': req.params.id, 'from.name': req.params.name })
     .populate('creator', 'name google')
+    .sort({
+      createAt: 'desc'
+    })
     .exec((err, items) => {
       if (err) {
         res.status(500).send({ msg: err });
