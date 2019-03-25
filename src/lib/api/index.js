e2338d86c639535c8b38ebe0992fb8e87eac2f46
Simon Gomes <xtgomes@gmail.com>
2019-03-25 11:56:51 -0300

    change url

--- src/lib/api/index.js
+++ src/lib/api/index.js
@@ -1,7 +1,7 @@
 const apis = new Map();
-apis.set('test', 'https://test.unabase.es');
-apis.set('dev', 'https://dev.unabase.es');
-apis.set('prod', 'https://api.unabase.es');
+apis.set('test', 'https://test.unabase.net');
+apis.set('dev', 'https://dev.unabase.net');
+apis.set('prod', 'https://api.unabase.net');
 apis.set('local', 'http://localhost:3000');
 
 let env = process.env.NODE_ENV === 'dest' ? 'test' : process.env.NODE_ENV;
