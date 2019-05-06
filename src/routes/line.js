7f7b8186a2f916031a79f70eaeed7794f8524c3c
Simon Gomes <xtgomes@gmail.com>
2019-05-06 18:04:19 -0400

    add updatemany lines to routes

--- src/routes/line.js
+++ src/routes/line.js
@@ -1,61 +1,64 @@
-import { Router } from 'express';
+import { Router } from "express";
 const lines = Router();
-import {
-  create,
-  get,
-  updateOne,
-  deleteOne,
-  createMany
-} from '../controllers/line';
+import { create, get, updateOne, deleteOne, createMany, updateMany } from "../controllers/line";
 
-import auth from '../config/lib/auth';
-import logger from '../lib/logger';
+import auth from "../config/lib/auth";
+import logger from "../lib/logger";
 
 lines.use(auth.sToken);
 
-let module = 'line';
+let module = "line";
 
 lines.get(
-  '/',
+  "/",
   logger({
-    name: 'list lines',
-    description: 'get the list of lines',
+    name: "list lines",
+    description: "get the list of lines",
     module
   }),
   get
 );
 lines.post(
-  '/',
+  "/",
   logger({
-    name: 'create line',
-    description: 'create line',
+    name: "create line",
+    description: "create line",
     module
   }),
   create
 );
 lines.post(
-  '/many',
+  "/many",
   logger({
-    name: 'create line',
-    description: 'create line',
+    name: "create line",
+    description: "create line",
     module
   }),
   createMany
 );
 lines.put(
-  '/:id',
+  "/many",
   logger({
-    name: 'updateOne lines',
-    description: 'updateOne line',
+    name: "create line",
+    description: "create line",
+    module
+  }),
+  updateMany
+);
+lines.put(
+  "/:id",
+  logger({
+    name: "updateOne lines",
+    description: "updateOne line",
     module
   }),
   updateOne
 );
 lines.delete(
-  '/:id',
+  "/:id",
   logger({
-    name: 'delete one line',
-    description: 'delete one line',
+    name: "delete one line",
+    description: "delete one line",
     module
   }),
   deleteOne
