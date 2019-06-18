84d5921f5e1c764895368be6bd937f7a3df82220
Simon Gomes <xtgomes@gmail.com>
2019-06-18 16:45:53 -0400

    field in comment model to make it private

--- src/models/comments.js
+++ src/models/comments.js
@@ -1,20 +1,21 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema(
   {
-    creator: { type: Schema.Types.ObjectId, ref: 'User' },
+    creator: { type: Schema.Types.ObjectId, ref: "User" },
     from: {
-      id: { type: Schema.Types.ObjectId, refPath: 'from.name' },
+      id: { type: Schema.Types.ObjectId, refPath: "from.name" },
       name: { type: String }
     },
+    private: Boolean,
     text: { type: String, maxlength: 2000 },
-    parent: { type: Schema.Types.ObjectId, ref: 'Comment' },
-    users: Array({ type: Schema.Types.ObjectId, ref: 'User' })
+    parent: { type: Schema.Types.ObjectId, ref: "Comment" },
+    users: Array({ type: Schema.Types.ObjectId, ref: "User" })
   },
   { timestamps: true }
 );
 
-const Comments = mongoose.model('Comment', commentSchema);
+const Comments = mongoose.model("Comment", commentSchema);
 
 export default Comments;
