d03865253b908e58dbb4d5c7f3dc8f43bb93842d
Simon Gomes <xtgomes@gmail.com>
2019-07-17 13:15:36 -0400

    enum for value field on comment

--- src/models/comments.js
+++ src/models/comments.js
@@ -12,7 +12,7 @@ const commentSchema = new Schema(
     text: { type: String, maxlength: 2000 },
     parent: { type: Schema.Types.ObjectId, ref: "Comment" },
     users: Array({ type: Schema.Types.ObjectId, ref: "User" }),
-    value: { type: Number },
+    value: { type: Number, enum: [1, 0, -1] },
     type: { type: String, enum: ["call", "meeting", "email", "task", "schedule"] }
   },
   { timestamps: true }
