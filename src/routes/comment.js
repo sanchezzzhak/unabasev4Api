ac27a79cdfdf9a28efc7173d036eaed43b81412b
Simon Gomes <xtgomes@gmail.com>
2019-07-30 16:27:12 -0400

    add update one route to comments

--- src/routes/comment.js
+++ src/routes/comment.js
@@ -1,12 +1,13 @@
-import { create, deleteOne, getFrom } from '../controllers/comment';
-import { Router } from 'express';
+import { create, deleteOne, getFrom, updateOne } from "../controllers/comment";
+import { Router } from "express";
 const comment = Router();
-let module = 'comment';
+let module = "comment";
 
-import auth from '../config/lib/auth';
+import auth from "../config/lib/auth";
 comment.use(auth.sToken);
-comment.post('/', create);
-comment.delete('/:id', deleteOne);
-comment.get('/:id/:name', getFrom);
+comment.post("/", create);
+comment.delete("/:id", deleteOne);
+comment.get("/:id/:name", getFrom);
+comment.put("/:id", updateOne);
 
 export default comment;
