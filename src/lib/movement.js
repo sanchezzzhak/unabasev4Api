693a2ebd60847a27c28b4ad341e162a69fbfa1c8
simon gomes <xtgomes@gmail.com>
2019-12-02 16:09:50 -0300

    calculateTotalMovement

--- src/lib/movement.js
+++ src/lib/movement.js
@@ -2,42 +2,43 @@ import Movement from "../models/movement";
 import Line from "../models/line";
 
 export const calculateTotalMovement = id => {
-  return new Promise((resolve, reject) => {
-    Movement.findById(id).exec(async (err, movement) => {
-      if (err) {
-        reject(err);
-      } else {
-        let lines = await Line.find({ movement: id })
-          .populate([{ path: "taxes.tax" }])
-          .exec();
-        let total = {
-          net: 0,
-          budget: 0,
-          tax: {
-            total: 0,
-            detail: []
-          }
-        };
-        lines.map(line => {
-          total.net += line.quantity * line.numbers.price;
-          total.budget += line.quantity * line.numbers.budget;
-          line.taxes.forEach(tax => {
-            total.tax.total += tax.price;
-            total.tax.detail.push({
-              name: tax.name,
-              number: tax.number
-            });
-          });
+  return new Promise(async (resolve, reject) => {
+    // Movement.findById(id).exec(async (err, movement) => {
+    //   if (err) {
+    //     reject(err);
+    //   } else {
+
+    //   }
+    // });
+    let lines = await Line.find({ movement: id })
+      .populate([{ path: "taxes.tax" }])
+      .exec();
+    let total = {
+      net: 0,
+      budget: 0,
+      tax: {
+        total: 0,
+        detail: []
+      }
+    };
+    lines.map(line => {
+      total.net += line.quantity * line.numbers.price;
+      total.budget += line.quantity * line.numbers.budget;
+      line.taxes.forEach(tax => {
+        total.tax.total += tax.price;
+        total.tax.detail.push({
+          name: tax.name,
+          number: tax.number
         });
-        total.profit = {
-          number: total.net - total.budget,
-          percentage: 100 - [total.budget / (total.net / 100)]
-        };
-        resolve({
-          movement: {
-            total
-          }
-        });
+      });
+    });
+    total.profit = {
+      number: total.net - total.budget,
+      percentage: 100 - [total.budget / (total.net / 100)]
+    };
+    resolve({
+      movement: {
+        total
       }
     });
   });
