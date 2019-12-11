57e9e6ac5a6cbc7873d88392004ba7523aa0e62a
simon gomes <xtgomes@gmail.com>
2019-12-11 15:34:49 -0300

    fix enum field on permission model

--- src/models/permission.js
+++ src/models/permission.js
@@ -8,7 +8,7 @@ let permissionSchema = Schema({
   name: { type: String, required: true },
   action: Enum[("create", "delete", "null", "update", "action", "read")],
   path: String,
-  type: Enum[("internal", "external")],
+  type: { type: String, enum: ["internal", "external"] },
   all: { type: Boolean, defaul: false }
 });
 // permissionSchema.plugin(AutoIncrement, { inc_field: 'id' });
