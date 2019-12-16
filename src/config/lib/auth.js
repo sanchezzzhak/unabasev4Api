1480976ca001d83f69064eb5a3bc6281e7dd53c7
simon gomes <xtgomes@gmail.com>
2019-12-16 18:01:05 -0300

    debug auth

--- src/config/lib/auth.js
+++ src/config/lib/auth.js
@@ -1,26 +1,22 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 // import mainConfig from '../main';
 
-import envar from '../../lib/envar';
-import User from '../../models/user';
+import envar from "../../lib/envar";
+import User from "../../models/user";
 export default {
   isAuth: (req, res, next) => {
-    if (
-      req.isAuthenticated() ||
-      req.method === 'OPTIONS' ||
-      req.headers.authorization === 'postmanvn4b4s3'
-    ) {
+    if (req.isAuthenticated() || req.method === "OPTIONS" || req.headers.authorization === "postmanvn4b4s3") {
       // if (req.isAuthenticated()) {
       next();
     } else {
-      res.status(403).send({ msg: 'Not authorized.' });
+      res.status(403).send({ msg: "Not authorized." });
     }
   },
   cToken: (req, res, next) => {
     req.token = req.cookies.access_token;
     jwt.verify(req.token, envar().SECRET, (err, decoded) => {
       if (err) {
-        res.status(403).send({ msg: 'Not authorized..' });
+        res.status(403).send({ msg: "Not authorized.." });
       } else {
         next();
       }
@@ -28,47 +24,42 @@ export default {
   },
   sToken: (req, res, next) => {
     req.token = req.headers.authorization;
-    // console.log(req.headers['authorization'].toString());
-    if (
-      typeof req.token !== 'undefined' &&
-      req.headers.authorization !== 'postmanvn4b4s3'
-    ) {
+    console.log("stoken");
+    console.log(req.headers["authorization"].toString());
+    if (typeof req.token !== "undefined" && req.headers.authorization !== "postmanvn4b4s3") {
       jwt.verify(req.token, envar().SECRET, (err, decoded) => {
         if (err) {
-          res.status(403).send({ msg: 'Not authorized1' });
+          res.status(403).send({ msg: "Not authorized1" });
         } else {
           req.user = decoded.user;
           next();
         }
       });
       // next();
-    } else if (req.method === 'OPTIONS') {
+    } else if (req.method === "OPTIONS") {
       next();
-    } else if (
-      req.headers.authorization === 'postmanvn4b4s3' ||
-      process.env.NODE_ENV === 'test'
-    ) {
+    } else if (req.headers.authorization === "postmanvn4b4s3" || process.env.NODE_ENV === "test") {
       let query;
-      if (req.headers['proxy-authorization']) {
-        query = { _id: req.headers['proxy-authorization'] };
+      if (req.headers["proxy-authorization"]) {
+        query = { _id: req.headers["proxy-authorization"] };
       } else {
         query = {
-          $or: [{ 'emails.email': { $regex: 'mail', $options: 'i' } }]
+          $or: [{ "emails.email": { $regex: "mail", $options: "i" } }]
         };
       }
       User.findOne(query).exec((err, user) => {
         if (err) {
-          console.log('err find');
+          console.log("err find");
           console.log(err);
         } else if (user) {
           req.user = user;
           next();
         } else {
-          res.status(404).send({ msg: 'Not user found for auth' });
+          res.status(404).send({ msg: "Not user found for auth" });
         }
       });
     } else {
-      res.status(403).send({ msg: 'Not authorized2' });
+      res.status(403).send({ msg: "Not authorized2" });
     }
   }
 };
