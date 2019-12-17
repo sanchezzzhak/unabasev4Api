d729f4a3d2795756f395ef8ffbe865ba2f05582d
simon gomes <xtgomes@gmail.com>
2019-12-17 17:00:48 -0300

    refactor checkGloba into promise

--- src/lib/checkGlobal.js
+++ src/lib/checkGlobal.js
@@ -7,40 +7,44 @@ export const checkGlobal = async movementId => {
   let movement = await Movement.findById(movementId).exec();
   let currencyString = movement.currency.toString();
   // new Promise((resolve, reject) => {
-  Line.find({ movement: movement._id })
-    .populate("item")
-    .exec(async (err, lines) => {
-      if (err) {
-        console.log(err);
-      } else {
-        let currencies;
-        await asyncForEach(lines, async line => {
-          currencies = Array.from(line.item.global.map(i => i.currency.toString()));
-          if (!currencies.includes(currencyString)) {
-            await Item.findById(line.item, (err, item) => {
-              item.global.push({
-                currency: movement.currency
+  return new Promise((resolve, reject) => {
+    Line.find({ movement: movement._id })
+      .populate("item")
+      .exec(async (err, lines) => {
+        if (err) {
+          console.log(err);
+          reject(err);
+        } else {
+          let currencies;
+          await asyncForEach(lines, async line => {
+            currencies = Array.from(line.item.global.map(i => i.currency.toString()));
+            if (!currencies.includes(currencyString)) {
+              await Item.findById(line.item, (err, item) => {
+                item.global.push({
+                  currency: movement.currency
+                });
+                item.save();
               });
-              item.save();
-            });
-          }
-        });
-        // lines.forEach(line => {
-        //   let currencies = Array.from(line.item.global.map(i => i.currency.toString()));
-        //   console.log("-----------------------currency from movement");
-        //   console.log(currencyString);
-        //   console.log("-----------------------currency from global");
-        //   console.log(currencies.includes(currencyString));
-        //   console.log(currencies);
-        //   if (!currencies.includes(currencyString)) {
-        //     Item.findById(line.item, (err, item) => {
-        //       item.global.push({
-        //         currency: movement.currency
-        //       });
-        //       item.save();
-        //     });
-        //   }
-        // });
-      }
-    });
+            }
+          });
+          resolve(true);
+          // lines.forEach(line => {
+          //   let currencies = Array.from(line.item.global.map(i => i.currency.toString()));
+          //   console.log("-----------------------currency from movement");
+          //   console.log(currencyString);
+          //   console.log("-----------------------currency from global");
+          //   console.log(currencies.includes(currencyString));
+          //   console.log(currencies);
+          //   if (!currencies.includes(currencyString)) {
+          //     Item.findById(line.item, (err, item) => {
+          //       item.global.push({
+          //         currency: movement.currency
+          //       });
+          //       item.save();
+          //     });
+          //   }
+          // });
+        }
+      });
+  });
 };
