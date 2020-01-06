fe6f44828e7febe60a28afddf084abd293e900cd
hector gonzalez <hectorluisgonzalezlarreal@gmail.com>
2020-01-06 15:48:23 -0300

    commit antes de hacer pull de os cambios de simon

--- src/controllers/userPermission.js
+++ src/controllers/userPermission.js
@@ -1,5 +1,7 @@
 import UserPermission from "../models/userPermission";
-import { queryHelper } from "../lib/queryHelper";
+import {
+  queryHelper
+} from "../lib/queryHelper";
 
 export const create = (req, res, next) => {
   UserPermission.create(req.body, (err, userPermission) => {
@@ -8,7 +10,9 @@ export const create = (req, res, next) => {
   });
 };
 export const deleteOneById = (req, res, next) => {
-  UserPermission.deleteOne({ _id: req.params.id }, err => {
+  UserPermission.deleteOne({
+    _id: req.params.id
+  }, err => {
     if (err) next(err);
     res.send({
       success: true
@@ -16,9 +20,17 @@ export const deleteOneById = (req, res, next) => {
   });
 };
 export const deleteOne = (req, res, next) => {
-  const { business, user, permission } = req.body;
+  const {
+    business,
+    user,
+    permission
+  } = req.body;
   if (user && business && permission) {
-    UserPermission.deleteOne({ business, user, permission }, err => {
+    UserPermission.deleteOne({
+      business,
+      user,
+      permission
+    }, err => {
       if (err) return next(err);
 
       res.send({
@@ -34,45 +46,47 @@ export const deleteOne = (req, res, next) => {
 };
 
 export const find = (req, res, next) => {
+  console.log('AAAAAAAAAAAAAAAAAAAAAAA', req.query)
   // TODO   function to check params
-  if (req.query.user && req.query.business) {
-    // let options = {};
-    // options.page = req.body.page || 1;
-    // options.limit = req.body.limit || 20;
-    // options.populate = [
-    //   {
-    //     path: "user",
-    //     select: "isActive name  email phone user imgUrl emails type"
-    //   },
-    //   {
-    //     path: "business",
-    //     select: "isActive name  email phone user imgUrl emails type"
-    //   },
-    //   { path: "permission" }
-    // ];
-    // delete req.body.page;
-    // delete req.body.limit;
-    let populate = [
-      {
-        path: "user",
-        select: "isActive name  email phone user imgUrl emails type"
-      },
-      {
-        path: "business",
-        select: "isActive name  email phone user imgUrl emails type"
-      },
-      { path: "permission" }
-    ];
-    let helper = queryHelper(req.query, { populate });
+  // if (req.query.user && req.query.business) {
+  // let options = {};
+  // options.page = req.body.page || 1;
+  // options.limit = req.body.limit || 20;
+  // options.populate = [
+  //   {
+  //     path: "user",
+  //     select: "isActive name  email phone user imgUrl emails type"
+  //   },
+  //   {
+  //     path: "business",
+  //     select: "isActive name  email phone user imgUrl emails type"
+  //   },
+  //   { path: "permission" }
+  // ];
+  // delete req.body.page;
+  // delete req.body.limit;
+  // let populate = [{
+  //     path: "user",
+  //     select: "isActive name  email phone user imgUrl emails type"
+  //   },
+  //   {
+  //     path: "business",
+  //     select: "isActive name  email phone user imgUrl emails type"
+  //   },
+  //   {
+  //     path: "permission"
+  //   }
+  // ];
+  let helper = queryHelper(req.query, {});
 
-    UserPermission.paginate(helper.query, helper.options, (err, userPermissions) => {
-      if (err) return next(err);
-      res.send(userPermissions);
-    });
-  } else {
-    let err = new Error();
-    err.statusCode = 406;
-    err.message = "data is not enough";
-    return next(err);
-  }
+  UserPermission.paginate(helper.query, helper.options, (err, userPermissions) => {
+    if (err) return next(err);
+    res.send(userPermissions);
+  });
+  // } else {
+  //   let err = new Error();
+  //   err.statusCode = 406;
+  //   err.message = "data is not enough";
+  //   return next(err);
+  // }
 };
