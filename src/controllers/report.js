51cde60f16f14568c848341dc33acf9c46308008
simon gomes <xtgomes@gmail.com>
2020-01-22 15:16:53 -0300

    add data to movements in main report

--- src/controllers/report.js
+++ src/controllers/report.js
@@ -5,7 +5,12 @@ import Lines from "../models/line";
 
 export const main = async (req, res, next) => {
   const users = await User.countDocuments({ isActive: true }).exec();
-  const movements = await Movement.countDocuments({ isActive: true }).exec();
+  const movements = {
+    total: await Movement.countDocuments({ isActive: true }).exec(),
+    opportunities: await Movement.countDocuments({ isActive: true, state: "opportunity" }).exec(),
+    budgets: await Movement.countDocuments({ isActive: true, state: "budget" }).exec(),
+    businesses: await Movement.countDocuments({ isActive: true, state: "business" }).exec()
+  };
   const lines = await Lines.countDocuments({ isActive: true, isParent: false }).exec();
   const businesses = await Business.countDocuments({ isActive: true }).exec();
   res.send({ users, movements, lines, businesses });
