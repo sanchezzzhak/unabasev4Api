19119dc3b1dff12d90f39efce29afb34228e8623
simon gomes <xtgomes@gmail.com>
2020-01-28 16:03:59 -0300

    add users by date to  main report

--- src/controllers/report.js
+++ src/controllers/report.js
@@ -4,14 +4,29 @@ import Business from "../models/business";
 import Lines from "../models/line";
 
 export const main = async (req, res, next) => {
-  const users = await User.countDocuments({ isActive: true }).exec();
+  // const users = await User.countDocuments({ isActive: true }).exec();
+  const users = {
+    total: await User.countDocuments({ isActive: true }).exec(),
+    byYear: {
+      2019: await User.countDocuments({ createdAt: { $gte: new Date(2019, 0, 1), $lte: new Date(2019, 11, 31) } }).exec(),
+      2020: await User.countDocuments({ createdAt: { $gt: new Date(2020, 0, 1), $lt: new Date(2020, 11, 31) } }).exec()
+    },
+    byMonth: {
+      2019: {},
+      2020: {}
+    }
+  };
+  for (let i = 0; i < 13; i++) {
+    users.byMonth[2019][i] = await User.countDocuments({ createdAt: { $gte: new Date(2019, i, 1), $lte: new Date(2019, i, 31) } }).exec();
+    users.byMonth[2020][i] = await User.countDocuments({ createdAt: { $gte: new Date(2020, i, 1), $lte: new Date(2020, i, 31) } }).exec();
+  }
   const movements = {
     total: await Movement.countDocuments({ isActive: true }).exec(),
     opportunities: await Movement.countDocuments({ isActive: true, state: "opportunity" }).exec(),
     budgets: await Movement.countDocuments({ isActive: true, state: "budget" }).exec(),
     businesses: await Movement.countDocuments({ isActive: true, state: "business" }).exec()
   };
-  const lines = await Lines.countDocuments({ isActive: true, isParent: false }).exec();
+  const lines = await Lines.countDocuments({ isParent: false }).exec();
   const businesses = await Business.countDocuments({ isActive: true }).exec();
   res.send({ users, movements, lines, businesses });
 };
