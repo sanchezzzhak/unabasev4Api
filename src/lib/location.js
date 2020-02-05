adddab7155f80f7a1f0b121ac976c2eff740c694
simon gomes <xtgomes@gmail.com>
2020-02-05 18:33:14 -0300

    debug location

--- src/lib/location.js
+++ src/lib/location.js
@@ -3,5 +3,8 @@ import axios from "axios";
 export const getLocationByIp = async req => {
   const idx = req.ip.lastIndexOf(":");
   const ip = req.ip.substring(idx + 1, req.ip.length);
+  console.log("---------------ip");
+  console.log(ip);
+  console.log(req.headers);
   return await axios(apiLocation + ip);
 };
