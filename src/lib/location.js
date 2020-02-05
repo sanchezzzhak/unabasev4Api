20ed9b6e720e00509276c2e6662241d0aa814ae4
simon gomes <xtgomes@gmail.com>
2020-02-05 18:47:56 -0300

    fix ip location

--- src/lib/location.js
+++ src/lib/location.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getLocationByIp = async req => {
   const idx = req.ip.lastIndexOf(":");
   const ip = req.headers["x-forwarded-for"];
-  console.log("---------------------------ip");
+  console.log("-x--------------------------ip");
   // console.log(ip);
   console.log(req.headers["x-forwarded-for"]);
   return await axios(apiLocation + ip);
