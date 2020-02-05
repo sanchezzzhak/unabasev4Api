5c05077b6c4d45407fdad7bd20fce4c8e76819dd
simon gomes <xtgomes@gmail.com>
2020-02-05 18:47:31 -0300

    fix ip location

--- src/controllers/log.js
+++ src/controllers/log.js
@@ -1,14 +1,14 @@
-import Log from '../models/log';
-import ntype from 'normalize-type';
+import Log from "../models/log";
+import ntype from "normalize-type";
 
 export default {
   create: (req, res) => {
     let log = new Log();
     Object.assign(log, req.body);
 
-    let idx = req.ip.lastIndexOf(':');
-    log.ip = req.ip.substring(idx + 1, req.ip.length);
-    log.userAgent = req.get('User-Agent');
+    // let idx = req.ip.lastIndexOf(':');
+    log.ip = req.headers["x-forwarded-for"];
+    log.userAgent = req.get("User-Agent");
     log.user = req.user._id;
     log.save((err, item) => {
       if (err) {
@@ -37,11 +37,7 @@ export default {
   find: (req, res) => {
     const { q } = req.param;
     const query = {
-      $or: [
-        { name: { $regex: q, $options: 'i' } },
-        { module: { $regex: q, $options: 'i' } },
-        { description: { $regex: q, $options: 'i' } }
-      ]
+      $or: [{ name: { $regex: q, $options: "i" } }, { module: { $regex: q, $options: "i" } }, { description: { $regex: q, $options: "i" } }]
     };
 
     Log.paginate(query, {}, (err, items) => {
@@ -59,7 +55,7 @@ export default {
       } else if (item) {
         res.send(item);
       } else {
-        res.status(404).send({ msg: 'item not found' });
+        res.status(404).send({ msg: "item not found" });
       }
     });
   }
--- src/lib/location.js
+++ src/lib/location.js
@@ -2,9 +2,9 @@ const apiLocation = "http://ip-api.com/json/";
 import axios from "axios";
 export const getLocationByIp = async req => {
   const idx = req.ip.lastIndexOf(":");
-  const ip = req.ip.substring(idx + 1, req.ip.length);
-  console.log("---------------ip");
-  console.log(ip);
-  console.log(req.headers);
+  const ip = req.headers["x-forwarded-for"];
+  console.log("---------------------------ip");
+  // console.log(ip);
+  console.log(req.headers["x-forwarded-for"]);
   return await axios(apiLocation + ip);
 };
