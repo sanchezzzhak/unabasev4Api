ed19befcb1d1e2907dc9f153a011f8e32a4bb14e
simon gomes <xtgomes@gmail.com>
2020-02-06 12:31:10 -0300

    refactor movement model to use string intead of object id

--- src/models/movement.js
+++ src/models/movement.js
@@ -19,11 +19,11 @@ const movementSchema = new Schema(
     client: {
       name: String,
       // save the user if the contact created or selected has a user reference.
-      user: { type: Schema.Types.ObjectId, ref: "User" },
+      user: { type: String, ref: "User" },
       // save the contact id
-      contact: { type: Schema.Types.ObjectId, ref: "Contact" },
+      contact: { type: String, ref: "Contact" },
       // save the business id if the user has businesses and one of them is selected
-      business: { type: Schema.Types.ObjectId, ref: "Business" },
+      business: { type: String, ref: "Business" },
       // if one business from the SII if seleted, we save the id and the name
       businessData: {
         id: { type: String },
@@ -31,31 +31,27 @@ const movementSchema = new Schema(
       }
     },
     responsable: {
-      // type and data DEPRECATED
-      // type: { type: String, enum: ["Business", "Contact", "User"] },
-      // data: { type: Schema.Types.ObjectId, refPath: "responsable.type" },
-      //------------
       // save a name for the client if no contact is created.
       name: String,
       // save the user if the contact created or selected has a user reference.
-      user: { type: Schema.Types.ObjectId, ref: "User" },
+      user: { type: String, ref: "User" },
       // save the contact id
-      contact: { type: Schema.Types.ObjectId, ref: "Contact" },
+      contact: { type: String, ref: "Contact" },
       // save the business id if the user has businesses and one of them is selected
-      business: { type: Schema.Types.ObjectId, ref: "Business" },
+      business: { type: String, ref: "Business" },
       // if one business from the SII if seleted, we save the id and the name
       businessData: {
         id: { type: String },
         name: { type: String }
       }
     },
-    editor: { type: Schema.Types.ObjectId, ref: "User" },
+    editor: { type: String, ref: "User" },
 
-    creator: { type: Schema.Types.ObjectId, ref: "User" },
+    creator: { type: String, ref: "User" },
 
     isBusiness: { type: Boolean, default: false },
-    lines: Array({ type: Schema.Types.ObjectId, ref: "Line" }),
-    comments: Array({ type: Schema.Types.ObjectId, ref: "Comment" }),
+    lines: Array({ type: String, ref: "Line" }),
+    comments: Array({ type: String, ref: "Comment" }),
     successPercentage: { type: Number },
     total: {
       opportunity: { type: Number, default: 0 },
