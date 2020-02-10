7ba55d797fb14e5f668b06b4c74023592e7987a4
simon gomes <xtgomes@gmail.com>
2020-02-10 16:46:20 -0300

    index for empresas model

--- src/models/empresa.js
+++ src/models/empresa.js
@@ -6,7 +6,7 @@ let empresaSchema = Schema({
   dv: { type: String },
   razon_social: { type: String }
 });
-
+empresaSchema.index({ rut: 1, razon_social: 1 });
 empresaSchema.plugin(mongoosePaginate);
 
 const empresa = mongoose.model("Empresa", empresaSchema);
