0b240437cd82a3a376dd3fe0683a2010fa917bd5
simon gomes <xtgomes@gmail.com>
2020-02-28 16:20:48 -0300

    validate params line

--- src/routes/line.js
+++ src/routes/line.js
@@ -3,6 +3,7 @@ const lines = Router();
 import { create, get, updateOne, deleteOne, createMany, updateMany, deleteMany, group, move, createParent, getLinesByMovement, requestBudget } from "../controllers/line";
 import { checkItem, updateMovementState, updateTotalMovement, updateItemLastPrice, checkParent, updateOldParent, getCurrencyFromMovement } from "../middleware/line";
 
+import { validateParams } from "../middleware/validate";
 import { sToken } from "../config/lib/auth";
 import logger from "../lib/logger";
 
@@ -11,7 +12,22 @@ lines.use(sToken);
 let module = "line";
 
 lines.get("/", logger({ name: "list lines", description: "get the list of lines", module }), get);
-lines.get("/movement/:movement", logger({ name: "get lines by movement", description: "get the list of lines", module }), getLinesByMovement);
+lines.get(
+  "/movement/:movement",
+  validateParams(
+    [
+      {
+        param_key: "movement",
+        required: true,
+        type: "string",
+        validator_functions: []
+      }
+    ],
+    "params"
+  ),
+  logger({ name: "get lines by movement", description: "get the list of lines", module }),
+  getLinesByMovement
+);
 lines.post(
   "/",
   [
@@ -23,6 +39,23 @@ lines.post(
     updateMovementState,
     checkParent
   ],
+  validateParams(
+    [
+      {
+        param_key: "currency",
+        required: true,
+        type: "string",
+        validator_functions: []
+      },
+      {
+        param_key: "name",
+        required: true,
+        type: "string",
+        validator_functions: []
+      }
+    ],
+    "body"
+  ),
   create
 );
 lines.post(
@@ -35,6 +68,23 @@ lines.post(
     }),
     updateMovementState
   ],
+  validateParams(
+    [
+      {
+        param_key: "parent",
+        required: true,
+        type: "object",
+        validator_functions: []
+      },
+      {
+        param_key: "children",
+        required: false,
+        type: "array",
+        validator_functions: []
+      }
+    ],
+    "body"
+  ),
   createParent
 );
 lines.put(
@@ -47,6 +97,35 @@ lines.put(
     }),
     updateOldParent
   ],
+  validateParams(
+    [
+      {
+        param_key: "movement",
+        required: true,
+        type: "string",
+        validator_functions: []
+      },
+      {
+        param_key: "parent",
+        required: false,
+        type: "string",
+        validator_functions: []
+      },
+      {
+        param_key: "oldParent",
+        required: false,
+        type: "string",
+        validator_functions: []
+      },
+      {
+        param_key: "children",
+        required: false,
+        type: "array",
+        validator_functions: []
+      }
+    ],
+    "body"
+  ),
   move
 );
 lines.post(
@@ -59,20 +138,48 @@ lines.post(
     }),
     checkItem
   ],
+  validateParams(
+    [
+      {
+        param_key: "children",
+        required: true,
+        type: "array",
+        validator_functions: []
+      }
+    ],
+    "body"
+  ),
   group
 );
 lines.post(
   "/many",
-  [
-    logger({
-      name: "create line",
-      description: "create line",
-      module
-    }),
-    updateMovementState,
-    checkParent,
-    getCurrencyFromMovement
-  ],
+
+  logger({
+    name: "create line",
+    description: "create line",
+    module
+  }),
+  updateMovementState,
+  checkParent,
+  getCurrencyFromMovement,
+
+  validateParams(
+    [
+      {
+        param_key: "lines",
+        required: true,
+        type: "array",
+        validator_functions: []
+      },
+      {
+        param_key: "movement",
+        required: true,
+        type: "string",
+        validator_functions: []
+      }
+    ],
+    "body"
+  ),
   createMany
 );
 lines.put(
@@ -82,6 +189,23 @@ lines.put(
     description: "create line",
     module
   }),
+  validateParams(
+    [
+      {
+        param_key: "lines",
+        required: true,
+        type: "array",
+        validator_functions: []
+      },
+      {
+        param_key: "data",
+        required: false,
+        type: "object",
+        validator_functions: []
+      }
+    ],
+    "body"
+  ),
   updateMany
 );
 lines.delete(
@@ -91,20 +215,64 @@ lines.delete(
     description: "create line",
     module
   }),
+  validateParams(
+    [
+      {
+        param_key: "totalMovement",
+        required: false,
+        type: "object",
+        validator_functions: []
+      },
+      {
+        param_key: "lines",
+        required: true,
+        type: "array",
+        validator_functions: []
+      }
+    ],
+    "body"
+  ),
   deleteMany
 );
 lines.put(
   "/:id",
-  [
-    logger({
-      name: "updateOne lines",
-      description: "updateOne line",
-      module
-    }),
-    updateTotalMovement,
-    updateItemLastPrice,
-    checkParent
-  ],
+  logger({
+    name: "updateOne lines",
+    description: "updateOne line",
+    module
+  }),
+  updateTotalMovement,
+  updateItemLastPrice,
+  checkParent,
+
+  validateParams(
+    [
+      {
+        param_key: "parent",
+        required: false,
+        type: "string",
+        validator_functions: []
+      },
+      {
+        param_key: "numbers",
+        required: false,
+        type: "object",
+        validator_functions: []
+      }
+    ],
+    "body"
+  ),
+  validateParams(
+    [
+      {
+        param_key: "id",
+        required: true,
+        type: "string",
+        validator_functions: []
+      }
+    ],
+    "params"
+  ),
   updateOne
 );
 lines.delete(
@@ -114,6 +282,17 @@ lines.delete(
     description: "delete one line",
     module
   }),
+  validateParams(
+    [
+      {
+        param_key: "id",
+        required: true,
+        type: "string",
+        validator_functions: []
+      }
+    ],
+    "params"
+  ),
   deleteOne
 );
 
@@ -124,6 +303,29 @@ lines.post(
     description: "request budget",
     module
   }),
+  validateParams(
+    [
+      {
+        param_key: "movement",
+        required: true,
+        type: "string",
+        validator_functions: []
+      },
+      {
+        param_key: "lines",
+        required: true,
+        type: "array",
+        validator_functions: []
+      },
+      {
+        param_key: "contact",
+        required: true,
+        type: "string",
+        validator_functions: []
+      }
+    ],
+    "body"
+  ),
   requestBudget
 );
 
