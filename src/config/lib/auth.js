82947726eb77e8a513d9d9400082330f256d24f4
simon gomes <xtgomes@gmail.com>
2020-03-02 12:28:10 -0300

    refactor sToken to have the last updated user

--- src/config/lib/auth.js
+++ src/config/lib/auth.js
@@ -29,14 +29,20 @@ export const cToken = (req, res, next) => {
 export const sToken = (req, res, next) => {
   req.token = req.headers.authorization;
   if (typeof req.token !== "undefined" && req.headers.authorization !== "postmanvn4b4s3") {
-    jwt.verify(req.token, envar().SECRET, (err, decoded) => {
+    jwt.verify(req.token, envar().SECRET, async (err, decoded) => {
       if (err) {
         res.status(403).send({
           msg: "Not authorized1"
         });
       } else {
         logy("decoded!");
-        req.user = decoded.user;
+        const authUser = await User.findById(decoded.user._id)
+          .select(
+            "isActive security.hasPassword security.isRandom isActive name username idNumber phones emails scope address imgUrl currency google.name google.email google.imgUrl contacts otherAccounts"
+          )
+          .lean();
+        authUser.id = authUser._id.toString();
+        req.user = authUser;
         next();
       }
     });
