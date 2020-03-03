fe7f8f22486544ddeb1fbfa6c7a96110469a240e
simon gomes <xtgomes@gmail.com>
2020-03-03 18:39:07 -0300

    send 201 status on webPushTest

--- src/controllers/push.js
+++ src/controllers/push.js
@@ -25,4 +25,6 @@ export const pushTest = (req, res, next) => {
 
   // Pass object into sendNotification
   webpush.sendNotification(req.user.webpush.subscription, payload).catch(err => console.error(err));
+
+  res.status(201).json({});
 };
