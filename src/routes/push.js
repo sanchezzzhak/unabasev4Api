6ddfcee7fcb11f437569eb701dca3df8a4e6b00b
simon gomes <xtgomes@gmail.com>
2020-03-03 18:36:52 -0300

    import validate params on push route

--- src/routes/push.js
+++ src/routes/push.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 const router = Router();
 import { subscribe, pushTest } from "../controllers/push";
+import { validateParams } from "../middleware/validate";
 
 router.post(
   "/subscribe",
