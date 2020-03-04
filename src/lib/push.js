5cf11caab9714d23f2991dd02caa804e4e4a5de4
simon gomes <xtgomes@gmail.com>
2020-03-04 17:44:18 -0300

    debug push send

--- src/lib/push.js
+++ src/lib/push.js
@@ -1,6 +1,11 @@
 export const sendPush = (payload, user) => {
   payload = JSON.stringify(payload);
-  console.log(user.name + " have been notify");
+  try {
+    console.log(user);
+    console.log(user.name + " have been notify");
+  } catch (err) {
+    console.log(err);
+  }
   // Pass object into sendNotification
   webPush.sendNotification(user.webpush.subscription, payload).catch(err => console.error(err));
 };
