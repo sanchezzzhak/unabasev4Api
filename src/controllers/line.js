6e332033faf05787a9b4b938748cc33480adca5d
simon gomes <xtgomes@gmail.com>
2020-03-05 16:58:04 -0300

    create clientLine from outcome

--- src/controllers/line.js
+++ src/controllers/line.js
@@ -89,6 +89,7 @@ export function createMany(req, res, next) {
   for (let line of req.body.lines) {
     line.creator = req.user._id;
   }
+
   // TODO check if all the lines are getting the creator data
   Line.insertMany(req.body.lines, async (err, lines) => {
     if (err) return next(err);
@@ -99,29 +100,19 @@ export function createMany(req, res, next) {
       }
     ]);
 
-    // checkGlobal(req.body.movement)
-    //   .then(_ => {
-    //   })
-    //   .catch(err => {
-    //     return next(err);
-    //   });
-
-    // ------ se debe copiar el impuesto y valores del item en catalogo a cada linea. ------------
-
-    // ------ se debe calcular los totales del movimiento ------------
-
-    // deprecated, taxes must live in the lines model from the invoice document, not in the item model
-    // lines.forEach(line => {
-    //   let global = line.item.global.filter(i => i.currency.toString() === req.currency._id.toString());
-    //   line.numbers.price = global[0].estimate.sell.price.isActive ? global[0].estimate.sell.price.number : 0;
-    //   global[0].taxes.forEach(tax => {
-    //     line.taxes.push({
-    //       tax: tax._id,
-    //       price: line.quantity * line.numbers.price * (tax.number / 100)
-    //     });
-    //   });
-    //   line.save();
-    // });
+    if (req.body.clientLine) {
+      for await (let line of lines) {
+        let newLine = new Line({
+          creator: req.user._id,
+          item: line.item,
+          name: line.name,
+          "numbers.cost": line.numbers.price * line.quantity
+        });
+        let clientLine = await newLine.save();
+        line.clientLine = clientLine._id.toString();
+        await line.save();
+      }
+    }
     calculateTotalMovement(req.body.movement)
       .then(movement => {
         res.send({ lines, movement });
@@ -485,6 +476,7 @@ export async function move(req, res, next) {
 
 export async function updateOne(req, res, next) {
   const parentToUpdate = req.body.parent;
+
   let line = await Line.findOneAndUpdate({ _id: req.params.id }, req.body).exec();
 
   if (line) {
