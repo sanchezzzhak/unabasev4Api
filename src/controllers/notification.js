c3a58c5b846cfcc8ca7a7caccd98e56678309e13
simon gomes <xtgomes@gmail.com>
2020-03-06 12:58:36 -0300

    notifications controllers

---
+++ src/controllers/notification.js
@@ -0,0 +1,31 @@
+import Notification from "../models/notifications";
+import { queryHelper } from "../lib/queryHelper";
+
+export const get = async (req, res, next) => {
+  let populate = [
+    {
+      path: "movement",
+      select: "name"
+    },
+    {
+      path: "user",
+      select: "name"
+    }
+  ];
+  let helper = queryHelper(req.query, { populate });
+  try {
+    let notifications = await Notification.paginate(helper.query, helper.options);
+    res.send(notifications);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const setRead = async (req, res, next) => {
+  try {
+    let notification = await Notification.findByIdAndUpdate(req.params.id, { isRead: req.params.isRead }).lean();
+    res.send(notification);
+  } catch (err) {
+    next(err);
+  }
+};
