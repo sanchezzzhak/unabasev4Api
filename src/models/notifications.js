ac42c7e53937477a842f2e279c74b11ff56ec81f
simon gomes <xtgomes@gmail.com>
2020-03-06 12:48:03 -0300

    notification model

---
+++ src/models/notifications.js
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+import mongoosePaginate from "mongoose-paginate-v2";
+import paginateConfig from "../config/paginate";
+
+const notificationSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    body: { type: String },
+    user: { type: String, ref: "User" },
+    users: Array({ type: String, ref: "User" }),
+    link: { type: String },
+    isRead: { type: Boolean, default: false },
+    movement: { type: String, ref: "Movement" }
+  },
+  { timestamps: true }
+);
+
+notificationSchema.plugin(mongoosePaginate);
+mongoosePaginate.paginate.options = paginateConfig;
+
+const Notification = mongoose.model("Notification", notificationSchema);
+
+export default Notification;
