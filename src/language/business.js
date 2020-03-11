3acc193da9dd8e65e92f2e6c9a43e65aaeaeae8c
simon gomes <xtgomes@gmail.com>
2020-03-11 10:46:24 -0300

    add language for business

---
+++ src/language/business.js
@@ -0,0 +1,10 @@
+export default {
+  es: {
+    alreadyExist: "Empresa ya existe.",
+    notExist: "Empresa no existe."
+  },
+  en: {
+    alreadyExist: "Company already exists.",
+    notExist: "Company does not exists."
+  }
+};
