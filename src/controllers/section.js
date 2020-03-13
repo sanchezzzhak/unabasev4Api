afb3dd47635a4b34730cf85005d7fbde132d15ed
simon gomes <xtgomes@gmail.com>
2020-03-13 15:32:34 -0300

    section controllers

--- src/controllers/section.js
+++ src/controllers/section.js
@@ -1,2 +1,38 @@
+import Section from "../models/section";
 
+export const get = async (req, res, next) => {
+  try {
+    let sections = await Section.paginate({}).lean();
+    res.send(sections);
+  } catch (err) {
+    next(err);
+  }
+};
 
+export const getOne = async (req, res, next) => {
+  try {
+    let section = await Section.findById(req.params.id).lean();
+    res.send(section);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const updateOne = async (req, res, next) => {
+  try {
+    let section = await Section.findByIdAndUpdate(req.params.id, req.body).lean();
+    res.send(section);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const create = async (req, res, next) => {
+  try {
+    let section = new Section(req.body);
+    await section.save();
+    res.send(section);
+  } catch (err) {
+    next(err);
+  }
+};
