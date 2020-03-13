12cda89642d139cbd9d9c598e1e9f221c961147a
simon gomes <xtgomes@gmail.com>
2020-03-13 17:11:04 -0300

    itemAlias controllers

---
+++ src/controllers/itemAlias.js
@@ -0,0 +1,38 @@
+import ItemAlias from "../models/itemAlias";
+
+export const get = async (req, res, next) => {
+  try {
+    let itemAlias = await ItemAlias.paginate({}).lean();
+    res.send(itemAlias);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getOne = async (req, res, next) => {
+  try {
+    let itemAlias = await ItemAlias.findById(req.params.id).lean();
+    res.send(itemAlias);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const updateOne = async (req, res, next) => {
+  try {
+    let itemAlias = await ItemAlias.findByIdAndUpdate(req.params.id, req.body).lean();
+    res.send(itemAlias);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const create = async (req, res, next) => {
+  try {
+    let itemAlias = new ItemAlias(req.body);
+    await itemAlias.save();
+    res.send(itemAlias);
+  } catch (err) {
+    next(err);
+  }
+};
