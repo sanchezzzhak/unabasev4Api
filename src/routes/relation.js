e031e8ceeb7197f37ed0b6385482eae3268314dd
simon gomes <xtgomes@gmail.com>
2020-03-20 11:29:24 -0300

    relation routes

---
+++ src/routes/relation.js
@@ -0,0 +1,62 @@
+import { Router } from "express";
+import { sToken } from "../config/lib/auth";
+import { validateParams } from "../middleware/validate";
+import { create, deleteOne, get, stateChange } from "../controllers/relation";
+const router = Router();
+
+const module = "relation";
+router.use(sToken);
+
+router.get("/", get);
+router.post(
+  "/",
+  validateParams(
+    [
+      {
+        param_key: "receptor",
+        required: true,
+        type: "string"
+      }
+    ],
+    "body"
+  ),
+  create
+);
+router.delete(
+  "/:id",
+  validateParams(
+    [
+      {
+        param_key: "id",
+        required: true,
+        type: "string"
+      }
+    ],
+    "params"
+  ),
+  deleteOne
+);
+router.put(
+  "/state/:state",
+  validateParams(
+    [
+      {
+        param_key: "petitioner",
+        required: true,
+        type: "string"
+      }
+    ],
+    "body"
+  ),
+  validateParams(
+    [
+      {
+        param_key: "state",
+        required: true,
+        type: "string"
+      }
+    ],
+    "params"
+  ),
+  stateChange
+);
