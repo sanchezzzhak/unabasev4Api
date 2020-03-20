45b949fd3d9d5c31d56c82fd81d1d90221332a8d
simon gomes <xtgomes@gmail.com>
2020-03-20 11:47:28 -0300

    relation test create

--- __tests__/relation.test.js
+++ __tests__/relation.test.js
@@ -1,6 +1,7 @@
 import request from "./request";
 import userData from "../lib/user/data";
 import Relation from "../src/models/relation";
+import User from "../src/models/user";
 let authUser = "";
 
 before(done => {
@@ -16,10 +17,26 @@ before(done => {
 });
 
 describe("****   RELATION   ****", () => {
+  it("CREATE a create@relation", done => {
+    let user = new User(userData());
+    user.save(err => {
+      request
+        .post("/relations")
+        .set("authorization", authUser.token)
+        .send({
+          receptor: user._id.toString()
+        })
+        .end((err, res) => {
+          if (err) done(err);
+          res.status.should.equal(200);
+          res.body.should.be.a("object");
+          done();
+        });
+    });
+  });
   it("LIST all users get@relation", done => {
     request
-      // .get("/relations?petioner=" + authUser.user._id.toString())
-      .get("/relations")
+      .get("/relations?petioner=" + authUser.user._id.toString())
       .set("authorization", authUser.token)
 
       .end((err, res) => {
