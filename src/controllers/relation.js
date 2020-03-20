95e9d5cbf538ba2dc131ee39a590bed293819cd8
simon gomes <xtgomes@gmail.com>
2020-03-20 11:23:58 -0300

    relation controller

--- src/controllers/relation.js
+++ src/controllers/relation.js
@@ -1,4 +1,5 @@
 import Relation from "../models/relation";
+import { queryHelper } from "../lib/queryHelper";
 
 export const create = async (req, res, next) => {
   let relation = new Relation({
@@ -8,6 +9,42 @@ export const create = async (req, res, next) => {
   try {
     await relation.save();
     res.send(relation);
+  } catch (err) {
+    next(err);
+  }
+};
+export const stateChange = async (req, res, next) => {
+  try {
+    let relation = await Relation.findOneAndUpdate({ receptor: req.user.id, petitioner: req.body.petitioner, isActive: req.params.state }).exec();
+    res.send(relation);
+  } catch (err) {
+    next(err);
+  }
+};
+export const deleteOne = async (req, res, next) => {
+  try {
+    await Relation.findByIdAndDelete(req.params.id).exec();
+    res.send({ success: true });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const get = async (req, res, next) => {
+  let populate = [
+    {
+      path: "receptor",
+      select: "name"
+    },
+    {
+      path: "petitioner",
+      select: "name"
+    }
+  ];
+  let helper = queryHelper(req.query, { populate });
+  try {
+    let relations = await Relation.paginate(helper.query, helper.options).then({});
+    res.send(relations);
   } catch (err) {
     next(err);
   }
