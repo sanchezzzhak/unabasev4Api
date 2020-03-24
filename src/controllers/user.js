ab5617e1834054952de108916ef44f7210aac9d3
simon gomes <xtgomes@gmail.com>
2020-03-24 11:49:05 -0300

    return relations count on get username controller

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -497,9 +497,10 @@ export const getUsername = async (req, res, next) => {
       .exec();
     let incomes = await Movement.find({ "responsable.user": user.id, isActive: true, state: "business" }).countDocuments();
     let outcomes = await Movement.find({ "client.user": user.id, isActive: true, state: "business" }).countDocuments();
+    let relations = await Relation.find({ $or: [{ petitioner: user.id }, { receptor: user.id }] }, { isActive: true }).countDocuments();
     let relation = await Relation.findOne({ petitioner: req.user.id, receptor: user.id }, { isActive: 1 }).lean();
     if (!user) next(notFoundError());
-    res.send({ user, relation, incomes, outcomes });
+    res.send({ user, relation, relations, incomes, outcomes });
   } catch (err) {
     next(err);
   }
