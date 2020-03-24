1200787d505d9a72f79ca2b68c53d332908f635d
simon gomes <xtgomes@gmail.com>
2020-03-24 12:53:18 -0300

    section route find

--- src/routes/section.js
+++ src/routes/section.js
@@ -2,13 +2,27 @@ import { Router } from "express";
 import { sToken } from "../config/lib/auth";
 import logger from "../lib/logger";
 import { validateParams } from "../middleware/validate";
-import { get, getOne, updateOne, create } from "../controllers/section";
+import { get, getOne, updateOne, create, find } from "../controllers/section";
 let module = "section";
 
 const router = Router();
 router.use(sToken);
 
 router.get("/", get);
+router.get(
+  "/:id",
+  validateParams(
+    [
+      {
+        param_key: "id",
+        required: true,
+        type: "string"
+      }
+    ],
+    "params"
+  ),
+  find
+);
 router.get(
   "/:id",
   validateParams(
