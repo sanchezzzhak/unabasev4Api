a4b480ade17905acb0e301e998bb41cf1af91b4b
simon gomes <xtgomes@gmail.com>
2020-04-02 17:38:39 -0300

    link model

---
+++ src/models/link.js
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+import mongoosePaginate from "mongoose-paginate-v2";
+import mongooseConfig from "../config/paginate";
+
+const linkSchema = Schema(
+  {
+    user: { type: String, ref: "User" },
+    url: { type: String },
+    name: { type: String },
+    type: { type: String, enum: ["image", "video"] },
+    members: Array({
+      user: { type: String, ref: "User" },
+      contact: { type: String, ref: "Contact" },
+      positions: Array({ type: String, ref: "Section" })
+    })
+  },
+  { timestamps: true }
+);
+
+linkSchema.plugin(mongoosePaginate);
+
+const Link = mongoose.model("Link", linkSchema);
+mongoosePaginate.paginate.options = mongooseConfig;
+export default Link;
