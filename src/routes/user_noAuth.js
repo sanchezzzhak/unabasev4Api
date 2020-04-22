08427d6ca30758582cb36e5dc2af35bf184fe445
simon gomes <12417560+simongcs@users.noreply.github.com>
2020-04-22 15:37:05 -0400

    route in user_noAuth for findByEmail

--- src/routes/user_noAuth.js
+++ src/routes/user_noAuth.js
@@ -1,7 +1,22 @@
 import { Router } from "express";
-import { getUsername } from "../controllers/user";
+import { getUsername, findByEmail } from "../controllers/user";
+import { validateParams } from "../middleware/validate";
 const router = Router();
 let module = "user";
 router.get("/users/profile/:username", getUsername);
+router.get(
+  "/users/find/email/:email",
+  validateParams(
+    [
+      {
+        param_key: "email",
+        required: false,
+        type: "string"
+      }
+    ],
+    "params"
+  ),
+  findByEmail
+);
 
 export default router;
