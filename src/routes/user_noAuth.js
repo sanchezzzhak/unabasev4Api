84942f38845f5023288f495578c9bf83e57e4616
simon gomes <12417560+simongcs@users.noreply.github.com>
2020-04-22 16:47:54 -0400

    validateParam for /users/profile/:username

--- src/routes/user_noAuth.js
+++ src/routes/user_noAuth.js
@@ -3,7 +3,20 @@ import { getUsername, findByEmail } from "../controllers/user";
 import { validateParams } from "../middleware/validate";
 const router = Router();
 let module = "user";
-router.get("/users/profile/:username", getUsername);
+router.get(
+  "/users/profile/:username",
+  validateParams(
+    [
+      {
+        param_key: "username",
+        required: false,
+        type: "string"
+      }
+    ],
+    "params"
+  ),
+  getUsername
+);
 router.get(
   "/users/find/email/:email",
   validateParams(
