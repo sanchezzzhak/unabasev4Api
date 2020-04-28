91d5e50ab1ceb537df0ebfb05be2c966815cdd56
simon gomes <12417560+simongcs@users.noreply.github.com>
2020-04-28 14:00:31 -0400

    update db

--- src/db.js
+++ src/db.js
@@ -6,13 +6,8 @@ import logy from "./config/lib/logy";
 import envar from "./lib/envar";
 logy(envar());
 const dbConfig = {
-  // dev: `mongodb://${main.host}:27020/unabase`, // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
   dev: `mongodb+srv://${envar().DB_USER}:${envar().DB_PASS}@${envar().DB_DEV}`,
-  dev4: `mongodb+srv://${envar().DB_USER4}:${envar().DB_PASS4}@${envar().DB_DEV4}`,
-  una: `mongodb+srv://${envar().DB_USER}:${envar().DB_PASS}@${envar().DB_UNA}`,
-  test: `mongodb+srv://${envar().DB_USER}:${envar().DB_PASS}@${envar().DB_TEST}`,
-  local: `mongodb+srv://${envar().DB_USER}:${envar().DB_PASS}@${envar().DB_TEST}`,
-  prod: `mongodb+srv://${envar().DB_USER}:${envar().DB_PASS}@${envar().DB_PROD}`
+  test: `mongodb+srv://${envar().DB_USER}:${envar().DB_PASS}@${envar().DB_TEST}`
 };
 
 const env = process.env.NODE_ENV || "";
