bf4f8d5540b698568c227f22414ca3cc5d3ec9d5
simon gomes <12417560+simongcs@users.noreply.github.com>
2020-05-05 14:23:09 -0400

    refactor stoken to isAuth

--- src/config/lib/auth.js
+++ src/config/lib/auth.js
@@ -5,36 +5,60 @@ import envar from "../../lib/envar";
 import User from "../../models/user";
 import { createError } from "../../lib/error";
 
-export const isAuth = (req, res, next) => {
-  if (req.isAuthenticated() || req.method === "OPTIONS" || req.headers.authorization === "postmanvn4b4s3") {
-    // if (req.isAuthenticated()) {
-    next();
-  } else {
-    res.status(403).send({
-      msg: "Not authorized."
-    });
-  }
+// funcs to decoded, encoded and generate accessToken
+const decodeFunc = token => {
+  return jwt.verify(token, envar().SECRET);
 };
-export const cToken = (req, res, next) => {
-  req.token = req.cookies.access_token;
-  jwt.verify(req.token, envar().SECRET, (err, decoded) => {
-    if (err) {
-      res.status(403).send({
-        msg: "Not authorized.."
-      });
-    } else {
-      next();
-    }
-  });
+const encodeFunc = (toSing, expiresIn) => {
+  console.log("toSing");
+  console.log(toSing);
+  return jwt.sign(toSing, envar().SECRET, { expiresIn });
 };
+const accessToken = (user, tokenFresh = false) => {
+  console.log("user --- ");
+  console.log(user);
+  return encodeFunc({ ...user, tokenFresh }, "1d");
+};
+
+// export the functions to decoded, encoded and generate accessToken
+export const decode = decodeFunc;
+export const encode = encodeFunc;
+export const accessTokenGen = accessToken;
+
+// export the functions to generate refresh_token and get access with the refresh_token
+export const refreshTokenGen = user => {
+  return encodeFunc(user, "60d");
+};
+export const getTokenByRefresh = refresh => {
+  let payload = decodeFunc(refresh);
+  delete payload.iat;
+  delete payload.exp;
+  delete payload.nbf;
+  delete payload.jti;
+  console.log("decoded payload");
+  console.log(payload);
+  return accessToken(payload);
+};
+
+// export const isAuth = (req, res, next) => {
+//   if (req.isAuthenticated() || req.method === "OPTIONS" || req.headers.authorization === "postmanvn4b4s3") {
+//     // if (req.isAuthenticated()) {
+//     next();
+//   } else {
+//     res.status(403).send({
+//       msg: "Not authorized."
+//     });
+//   }
+// };
+
 export const isAuthOptional = async (req, res, next) => {
-  req.token = req.headers.authorization;
-  if (typeof req.token !== "undefined" && req.token !== "") {
-    let decoded = jwt.verify(req.token, envar().SECRET);
+  req.access_token = req.headers.authorization;
+  if (typeof req.access_token !== "undefined" && req.access_token !== "") {
+    let decoded = decodeFunc(req.access_token);
     let authUser;
 
     try {
-      authUser = await User.findOne({ _id: decoded.user._id })
+      authUser = await User.findOne({ _id: decoded._id })
         .select(
           "isActive webpush security.hasPassword security.isRandom isActive name username idNumber phones emails scope address imgUrl currency google.name google.email google.imgUrl contacts otherAccounts"
         )
@@ -45,7 +69,7 @@ export const isAuthOptional = async (req, res, next) => {
       req.user = authUser;
       next();
     } catch (err) {
-      next(createError(401, "Not authorized1"));
+      next(createError(401, "Not authorized."));
     }
     // next();
   } else if (req.method === "OPTIONS") {
@@ -54,14 +78,14 @@ export const isAuthOptional = async (req, res, next) => {
     next();
   }
 };
-export const sToken = async (req, res, next) => {
-  req.token = req.headers.authorization;
-  if (typeof req.token !== "undefined" && req.token !== "") {
-    let decoded = jwt.verify(req.token, envar().SECRET);
-    let authUser;
 
+export const isAuth = async (req, res, next) => {
+  req.access_token = req.headers.authorization;
+  if (typeof req.access_token !== "undefined" && req.access_token !== "") {
+    let decoded = decodeFunc(req.access_token);
+    let authUser;
     try {
-      authUser = await User.findOne({ _id: decoded.user._id })
+      authUser = await User.findOne({ _id: decoded._id })
         .select(
           "isActive webpush security.hasPassword security.isRandom isActive name username idNumber phones emails scope address imgUrl currency google.name google.email google.imgUrl contacts otherAccounts"
         )
@@ -72,14 +96,15 @@ export const sToken = async (req, res, next) => {
       req.user = authUser;
       next();
     } catch (err) {
-      next(createError(401, "Not authorized1"));
+      console.log(err);
+      next(createError(401, "Not authorized."));
     }
     // next();
   } else if (req.method === "OPTIONS") {
     next();
   } else {
-    res.status(403).send({
-      msg: "Not authorized2"
+    res.status(401).send({
+      msg: "Not authorized, client must send an access token."
     });
   }
 };
