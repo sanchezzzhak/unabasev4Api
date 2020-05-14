1d7e1adea3d8e3ac27cfea72d7f5beab5a6618a1
simon gomes <12417560+simongcs@users.noreply.github.com>
2020-05-14 18:13:32 -0400

    change json to js on files config

---
+++ src/config/files.js
@@ -0,0 +1,9 @@
+export default {
+    maxSize: {
+        profile: 5242880
+    },
+    profile: {
+        maxsize: 5242880,
+        formats: ["image/jpg", "image/jpeg", "image/png", "image/svg"]
+    }
+};
