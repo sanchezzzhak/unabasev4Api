d8de3c32e0b5e726ffdf253204e831bb2ed195c6
simon gomes <12417560+simongcs@users.noreply.github.com>
2020-05-19 19:42:42 -0400

    add isAuthOptional middleware to user_noAuth route

--- src/routes/user_noAuth.js
+++ src/routes/user_noAuth.js
@@ -1,36 +1,39 @@
 import { Router } from "express";
 import { getUsername, findByEmail } from "../controllers/user";
 import { validateParams } from "../middleware/validate";
+import { isAuthOptional } from "../config/lib/auth";
 const router = Router();
 let module = "user";
 
 router.get(
-  "/users/profile/:username",
-  validateParams(
-    [
-      {
-        param_key: "username",
-        required: false,
-        type: "string"
-      }
-    ],
-    "params"
-  ),
-  getUsername
+    "/users/profile/:username",
+    isAuthOptional,
+    validateParams(
+        [
+            {
+                param_key: "username",
+                required: false,
+                type: "string"
+            }
+        ],
+        "params"
+    ),
+    getUsername
 );
 router.get(
-  "/users/find/email/:email",
-  validateParams(
-    [
-      {
-        param_key: "email",
-        required: false,
-        type: "string"
-      }
-    ],
-    "params"
-  ),
-  findByEmail
+    "/users/find/email/:email",
+    isAuthOptional,
+    validateParams(
+        [
+            {
+                param_key: "email",
+                required: false,
+                type: "string"
+            }
+        ],
+        "params"
+    ),
+    findByEmail
 );
 
 export default router;
