a86256e0ce45a0811db094a15d59cff954c9478d
hector gonzalez <hectorluisgonzalezlarreal@gmail.com>
2020-05-28 14:40:28 -0400

    28/05/2020 - 4

--- src/server.js
+++ src/server.js
@@ -67,9 +67,17 @@ app.use(xmlparser());
 app.use(cookieParser());
 
  app.use(cors());
- app.options('*', cors());
+//  app.options('*', cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
- 
+
+
 app.use(localeMiddleware());
 //load view engine
 app.set("views", path.join(__dirname, "./views"));
