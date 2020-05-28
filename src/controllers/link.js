b1b2095930fb0d7ec33c6addb6ef1bb1f272f94c
hector gonzalez <hectorluisgonzalezlarreal@gmail.com>
2020-05-28 19:31:24 -0400

    28/05/2020 - 9

--- src/controllers/link.js
+++ src/controllers/link.js
@@ -115,13 +115,24 @@ export const getOneByUrl = async (req, res, next) => {
 
 export const getRelated = async (req, res, next) => {
   try {
-    let select = "name imgUrl google.imgUrl emails phones address otherAccounts sections";
-    let relations = await Relation.find({ $or: [{ petitioner: req.user.id }, { receptor: req.user.id }], isActive: true }, { petitioner: 1, receptor: 1 }).lean();
-    let users = relations.map(relation => (relation.petitioner === req.user.id ? relation.receptor : relation.petitioner));
-    let links = await Link.find({ $or: [{ "members.user": { $in: users } }, { user: { $in: users } }], user: { $ne: req.user._id } })
-      .sort({ createdAt: -1 })
-      .populate([{ path: "members.user", select }, { path: "members.positions" }, { path: "user", select, populate: [{ path: "sections" }] }])
-      .lean();
+     let select = "name imgUrl google.imgUrl emails phones address otherAccounts sections";
+    // let relations = await Relation.find({ $or: [{ petitioner: req.user.id }, { receptor: req.user.id }], isActive: true }, { petitioner: 1, receptor: 1 }).lean();
+    // let users = relations.map(relation => (relation.petitioner === req.user.id ? relation.receptor : relation.petitioner));
+   
+    let limit = req.query.limit || 15
+    let page = req.query.page || 1
+
+    let links = await Link.paginate({}, { 
+    populate: [{ path: "members.user", select },
+     { path: "members.positions" }, { path: "user",
+      select, populate: [{ path: "sections" }] }],
+       limit,
+        page,
+         sort: { createdAt: -1 } }).then({}); 
+         //{ $or: [{ "members.user": { $in: users } }, { user: { $in: users } }], user: { $ne: req.user._id } }
+        // .sort({ createdAt: -1 })
+       // .populate([{ path: "members.user", select }, { path: "members.positions" }, { path: "user", select, populate: [{ path: "sections" }] }])
+      // .lean();
     res.send(links);
   } catch (err) {
     next(err);
