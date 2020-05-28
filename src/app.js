8d60cf9fc1d2d0d87f4b719cf68de42b06ce1a6d
hector gonzalez <hectorluisgonzalezlarreal@gmail.com>
2020-05-28 14:29:55 -0400

    28/05/2020 - 3

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 import server from "./server";
 import logy from "./config/lib/logy";
 
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+
 const port = process.env.PORT || 3000;
 
 const env = process.env.NODE_ENV || "";
 // //start server
 server.listen(port, () => {
-  logy("Server started on port " + port + " env: " + env);
+  logy("Server started on port -> " + port + " env: " + env);
 });
