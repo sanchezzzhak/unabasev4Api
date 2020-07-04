09f8a3e9b7d53bac73f36bd6541a107a942f706a
Tutik Alexsandr <sanchezzzhak@ya.ru>
2020-07-05 00:50:12 +0300

    feat(device) enhanced performance detect
    When creating an instance, yml files are sync loaded every time you create a new DeviceDetector object.

--- src/middleware/device.js
+++ src/middleware/device.js
@@ -1,9 +1,9 @@
 import deviceDetector from "node-device-detector";
+const detectorDevice = new deviceDetector();
 
 export const detector = (req, res, next) => {
   const userAgent = req.headers["user-agent"];
-  const detector = new deviceDetector();
-  const device = detector.detect(userAgent);
+  const device = detectorDevice.detect(userAgent);
   req.deviceDetected = device;
   next();
 };
